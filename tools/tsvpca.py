9dcdf55654386be773c4c13903c94de948a2f8da
Pontus Stenetorp <pontus@stenetorp.se>
2012-10-26 17:38:35 +0900

    Minor Python script to perform PCA on TSV files

---
+++ tools/tsvpca.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+'''
+Perform Principal Component Analysis for TSV input using Scikit-learn.
+
+Author:     Pontus Stenetorp    <pontus stenetorp se>
+Version:    2012-09-13
+'''
+
+from argparse import ArgumentParser, FileType
+from math import sqrt
+from sys import stderr, stdin, stdout
+
+from numpy.linalg import svd
+from sklearn.decomposition import PCA
+
+def _argparser():
+    argparser = ArgumentParser('PCA for TSV input using Scikit-learn')
+
+    argparser.add_argument('-i', '--input', type=FileType('r'), default=stdin,
+            help='input source (default: stdin)')
+    argparser.add_argument('-o', '--output', type=FileType('w'), default=stdout,
+            help='output (default: stdout)')
+    argparser.add_argument('-d', '--dimensionality', type=int, default=2,
+            help='target dimensionality')
+    argparser.add_argument('-s', '--dimensionality-search',
+            action='store_true', help=('Print the sorted absolute values of '
+                'the co-variance matrix, can be used to determine the '
+                'optimal number of dimensions'))
+    argparser.add_argument('-n', '--no-normalisation', action='store_true',
+            help="don't normalise the output to the range of [0,1]")
+
+    return argparser
+
+def main(args):
+    argp = _argparser().parse_args(args[1:])
+
+    # Read the data
+    data_list = []
+    for line_num, line in enumerate((l.rstrip('\n') for l in argp.input),
+            start=1):
+        try:
+            data_list.append([float(v) for v in line.split('\t')])
+        except ValueError:
+            print >> stderr, ('ERROR: unable to read line {0} "{1}"'
+                    ).format(line_num, line)
+            return -1
+
+    # Useful flag for analysis of optimal dimension
+    if argp.dimensionality_search:
+        sing_vals = svd(data_list, full_matrices=0, compute_uv=False)
+        prev_sing_val = None
+        print '\t'.join(('Line', 'Sing. value', 'Delta', ))
+        for sing_val_i, sing_val in enumerate(sorted(abs(sing_vals)), start=1):
+            print '\t'.join(unicode(v) for v in (sing_val_i, sing_val,
+                sing_val - prev_sing_val if prev_sing_val is not None else ''
+                ))
+            prev_sing_val = sing_val
+        return 0
+
+    # Run PCA
+    pca_conf = PCA(n_components=argp.dimensionality, copy=False)
+    res = pca_conf.fit_transform(data_list)
+
+    # Normalise the results?
+    if not argp.no_normalisation:
+        _, columns = res.shape
+        for column in xrange(columns):
+            res[:,column] -= res[:,column].min()
+            res[:,column] /= res[:,column].max()
+
+    # And print it all out...
+    for row in res:
+        argp.output.write('\t'.join(unicode(e) for e in row))
+        argp.output.write('\n')
+
+    return 0
+
+if __name__ == '__main__':
+    from sys import argv
+    exit(main(argv))
